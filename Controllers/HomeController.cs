43ef12690eedfadde6b0b1cd577c97aebb20d056
parkerkschmidt <parkerschmidt05@gmail.com>
2022-01-26 09:46:30 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mission4.Models;
+
+namespace mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private MovieResponseContext BlahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieResponseContext someName)
+        {
+            _logger = logger;
+            BlahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FilmForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FilmForm(MovieResponse response)
+        {
+            BlahContext.Add(response);
+            BlahContext.SaveChanges();
+
+            return View("Confirmation", response);
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieResponseContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace mission4.Models
+{
+    public class MovieResponseContext : DbContext
+    {
+        public MovieResponseContext(DbContextOptions<MovieResponseContext> options) : base(options)
+        {
+            // leave blank for now
+        }
+
+        public DbSet<MovieResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+
+                new MovieResponse
+                {
+                    MovieID = 1,
+                    Category = "Action/Adventure",
+                    Title = "Back to the Future",
+                    Year = 1985,
+                    Director = "Robert Zemeckis",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+
+                new MovieResponse
+                {
+                    MovieID = 2,
+                    Category = "Comedy",
+                    Title = "Hot Rod",
+                    Year = 2007,
+                    Director = "Akiva Schaffer",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+
+                new MovieResponse
+                {
+                    MovieID = 3,
+                    Category = "Comedy",
+                    Title = "Hoodwinked!",
+                    Year = 2005,
+                    Director = "Cory Edwards",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+
+
+                );
+            ;
+        }
+
+    }
+}
+
+
+
